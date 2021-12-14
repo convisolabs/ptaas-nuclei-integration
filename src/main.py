b0416331e97244c0426cd6646cad6c3e828df043
Lucasmick1 <lucas_29_costa@hotmail.com>
2021-12-14 16:20:37 -0300

    adicionando deployer ao main.py

--- src/main.py
+++ src/main.py
@@ -16,19 +16,22 @@ https://github.com@convisolabs/ptaas-nuclei-integration/blob/master/README.md
 
 from lib import ArgumentParser
 from lib import ConvisoNucleiIntegration
-
+from lib import Deployer 
 
 def main():
     # 1 read nuclei scan output
     integrationInterface = ConvisoNucleiIntegration.IntegrationInterface(
-        __arguments__.nuclei_output
+        __arguments__.nuclei_output,
+        __arguments__.project_id
     )
-
+  
     # 2 create reports
     integrationInterface.conviso_reports = integrationInterface.get_conviso_reports()
 
     # 3 deploy to flow
-    print(integrationInterface.conviso_reports)
+    deployer = Deployer.ReportDeployer( __arguments__.api_key)
+    deployer.create_notification(integrationInterface.conviso_reports)
+    # print(integrationInterface.conviso_reports)
     # deploy_report_in_batch(conviso_reports)
 
 
